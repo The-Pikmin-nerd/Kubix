3a74c02dd8dcfcb1ee66d77c32cf111dd9b700c0
Yel0wPikmin <nathanael.mone@outlook.fr>
2025-11-28 12:10:16 +0100

    Refactor collision scripts and add player sprite animation
    Moved the KolisonCynematicLok collision script to its own file and updated LevelOne.tscn to reference the new external script for all collision shapes. Enhanced Player.tscn by adding a scale-in animation to the player sprite on ready. Removed unused sprite.gd and its UID file.

---
+++ kubix/Senes/Levels/KolisonCynematicLok.tres.gd
@@ -0,0 +1,13 @@
+extends CollisionShape2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	await get_tree().create_timer(3).timeout
+	print("Konsole : Deleted Colisionshape cynematiclok Entiti")
+	queue_free()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
